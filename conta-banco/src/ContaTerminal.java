5b6d3586e46526af1cd7a1217a389150f2458b6b
Vinicius Borges <vinnepaul@gmail.com>
2025-03-31 17:10:31 -0300

    Adiciona funcionalidade de criação de conta bancária via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner; 
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = teclado.nextInt();
+        teclado.nextLine(); // Consumir a quebra de linha
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = teclado.nextLine();
+        
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = teclado.nextLine();
+        
+        System.out.println("Por favor, digite o saldo:");
+        double saldo =  Double.parseDouble(teclado.nextLine());
+        
+        teclado.close();
+        
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        
+        System.out.println(mensagem);
     }
 }
